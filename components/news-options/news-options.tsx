588193001e8f98d270dd0ee43b219d1e05d28844
tyange <usun16@gmail.com>
2022-12-26 22:27:55 +0900

    added divider

--- components/news-options/news-options.tsx
+++ components/news-options/news-options.tsx
@@ -5,7 +5,9 @@ const NewsOptions = () => {
   return (
     <div>
       <CountryOptions />
-      <div></div>
+      <div className="flex justify-center">
+        <div className="border-t-2 w-1/2"></div>
+      </div>
       <CategoryOptions />
     </div>
   );
